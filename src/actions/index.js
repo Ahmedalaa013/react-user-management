3d1f0b4ed9918a979c745f442b6bfdcf5a1228b6
Ahmed Naga <Ahmed.Naga@itworx.com>
2022-09-08 14:24:16 +0200

    handle first fetch error

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,5 +3,5 @@ import dummyApi from "../apis/dummyApi";
 export const fetchUsers = () => async (dispatch) => {
   const response = await dummyApi.get("/user");
 
-  dispatch({ type: "FETCH_USERS", payload: response.data });
+  dispatch({ type: "FETCH_USERS", payload: response.data.data });
 };
--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,45 +1,41 @@
-import React from "react";
+import { useEffect } from "react";
 import { connect } from "react-redux";
 import { fetchUsers } from "../actions";
 
-class UsersList extends React.Component {
-  componentDidMount() {
-    this.props.fetchUsers();
-  }
+const UsersList = (props) => {
+  useEffect(() => {
+    props.fetchUsers();
+  }, []);
 
-  renderList = () => {
-    return this.props.users.data.map((user, index) => {
-      return (
-        <tr key={index}>
-          <th scope="row">{index + 1}</th>
-          <td>{user.firstName}</td>
-          <td>{user.lastName}</td>
-          <td>{user.picture}</td>
-        </tr>
-      );
-    });
-  };
-  render() {
-    console.log(this.props.users.data);
+  const data = props.users.map((user, index) => {
     return (
-      <div className="container">
-        <div className="row">
-          <table className="table table-hover">
-            <thead>
-              <tr>
-                <th scope="col">#</th>
-                <th scope="col">First Name</th>
-                <th scope="col">Last Name</th>
-                <th scope="col">Picture url</th>
-              </tr>
-            </thead>
-            <tbody>{this.renderList()}</tbody>
-          </table>
-        </div>
-      </div>
+      <tr key={index}>
+        <th scope="row">{index + 1}</th>
+        <td>{user.firstName}</td>
+        <td>{user.lastName}</td>
+        <td>{user.picture}</td>
+      </tr>
     );
-  }
-}
+  });
+
+  return (
+    <div className="container">
+      <div className="row">
+        <table className="table table-hover">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">First Name</th>
+              <th scope="col">Last Name</th>
+              <th scope="col">Picture url</th>
+            </tr>
+          </thead>
+          <tbody>{data}</tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
 const mapStateToProps = (state) => {
   return { users: state.users };
 };
